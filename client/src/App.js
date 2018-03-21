64bee796686327567a4f47ac24094b44a98faebd
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-21 16:22:38 -0400

    set up proxy and fetch tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import TaskList from './components/TaskList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+    this.getTasks = this.getTasks.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  async getTasks () {
+    const tasks = await fetch('api/tasks');
+    console.log(await tasks.json());
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={(props) => <TaskList {...props} />} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+// componentDidMount() {
+//   if (this.state.currentPage === 'index') {
+//     fetch('/api/icecream')
+//       .then(res => res.json())
+//       .then(res => {
+//         this.setState({
+//           allIceCreams: res.data.icecreams,
+//           dataLoaded: true,
+//         });
+//       }).catch(err => console.log(err));
+//   } else if (this.state.currentPage === 'show' || this.state.currentPage === 'edit') {
+//     fetch(`/api/icecream/${this.state.currentId}`)
+//       .then(res => res.json())
+//       .then(res => {
+//         this.setState({
+//           currentIceCream: res.data.icecream,
+//           dataLoaded: true,
+//         })
+//       }).catch(err => console.log(err));
+//   } else if (this.state.currentPage === 'new') {
+//     this.setState({
+//       dataLoaded: true,
+//     })
+//   }
+// }
+       
