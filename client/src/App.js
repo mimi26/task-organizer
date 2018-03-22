08f97b10290fa594e7d040028c3cc4dcde0944c5
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-21 20:03:49 -0400

    add delete funcionality

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      tasks: []
     }
     this.getTasks = this.getTasks.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() {
@@ -17,15 +18,25 @@ class App extends Component {
   }
 
   async getTasks () {
-    const tasks = await fetch('api/tasks');
-    console.log(await tasks.json());
+    let tasks = await fetch('api/tasks');
+    tasks = await tasks.json();
+    await this.setState({ tasks });
+  }
+
+  async handleDelete(id) {
+    const deleteTask = await fetch(`/api/tasks/${id}`, {
+      method: 'DELETE'
+    });
+    this.getTasks();
   }
 
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={(props) => <TaskList {...props} />} />
+          <Route exact path="/" component={(props) => <TaskList {...props} 
+                                        tasks={this.state.tasks}
+                                        handleDelete={this.handleDelete} />} />
         </Switch>
       </BrowserRouter>
     );
@@ -33,30 +44,3 @@ class App extends Component {
 }
 
 export default App;
-
-// componentDidMount() {
-//   if (this.state.currentPage === 'index') {
-//     fetch('/api/icecream')
-//       .then(res => res.json())
-//       .then(res => {
-//         this.setState({
-//           allIceCreams: res.data.icecreams,
-//           dataLoaded: true,
-//         });
-//       }).catch(err => console.log(err));
-//   } else if (this.state.currentPage === 'show' || this.state.currentPage === 'edit') {
-//     fetch(`/api/icecream/${this.state.currentId}`)
-//       .then(res => res.json())
-//       .then(res => {
-//         this.setState({
-//           currentIceCream: res.data.icecream,
-//           dataLoaded: true,
-//         })
-//       }).catch(err => console.log(err));
-//   } else if (this.state.currentPage === 'new') {
-//     this.setState({
-//       dataLoaded: true,
-//     })
-//   }
-// }
-       
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class TaskList extends Component {
+    constructor() {
+        super();
+    }
+
+    // iceCreamDelete(id) {
+    //     fetch(`/api/icecream/${id}`, {
+    //         method: 'DELETE',
+    //     }).then(res => res.json())
+    //         .then(res => {
+    //             console.log(res);
+    //             this.setState({
+    //                 fireRedirect: true,
+    //                 redirectPath: '/ice-cream',
+    //             });
+    //         }).catch(err => console.log(err));
+    // }
+
     render() {
         return (
             <div>
-                hi
+                <h1>Things to do:</h1>
+                {this.props.tasks.map(task => {
+                    return(
+                        <div key={task.id}>
+                            {task.title}
+                            {task.task}
+                            <button>Edit Task</button>
+                            <button onClick={() => this.props.handleDelete(task.id)}>Delete Task</button>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
