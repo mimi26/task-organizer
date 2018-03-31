cebb056d3dd0385153f9cccc7bf2704ee0756e93
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-31 14:17:43 -0400

    change to config file

--- db/config.js
+++ db/config.js
@@ -9,16 +9,22 @@ const options = {
 
 const pgp = require('pg-promise')(options);
 
+// function setDatabase() {
+//     if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+//         return pgp({
+//             database: 'task_organizer_db',
+//             port: 5432,
+//             host: 'localhost'
+//         });
+//     } else if (process.env.NODE_ENV === 'production') {
+//         return pgp(process.env.DATABASE_URL);
+//     }
+// }
+
 function setDatabase() {
-    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-        return pgp({
-            database: 'task_organizer_db',
-            port: 5432,
-            host: 'localhost'
-        });
-    } else if (process.env.NODE_ENV === 'production') {
+    
         return pgp(process.env.DATABASE_URL);
-    }
+    
 }
 
 const db = setDatabase();
