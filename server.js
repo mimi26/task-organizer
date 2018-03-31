3a036d77554a11b1f3b5f5a454de94cc298f6232
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-31 17:37:16 -0400

    change to headers in server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 // }
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://modest-babbage-a37f04.bitballoon.com/");
+    res.header("Access-Control-Allow-Origin", "http://modest-babbage-a37f04.bitballoon.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
