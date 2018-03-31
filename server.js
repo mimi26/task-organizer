93c7bbbbd50c03f9933ab96f25c7675760118584
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-31 17:12:32 -0400

    remove app.use..client/build from server

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
