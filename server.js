06e82cc5e3cf6c34da21bc8c69e4c93036158369
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-04-04 11:31:37 -0400

    moved cors enabling above session

--- server.js
+++ server.js
@@ -23,17 +23,6 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser(process.env.SESSION_KEY));
 
-const session = require('express-session');
-app.use(session({
-    secret: process.env.SESSION_KEY,
-    resave: false,
-    saveUninitialized: true
-}));
-
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
-
 //enable cors.
 const cors = require('cors');
 app.use(cors());
@@ -45,6 +34,17 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
+
+const session = require('express-session');
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true
+}));
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 const taskRoutes = require('./routes/task-routes');
 app.use('/api/tasks', taskRoutes);
