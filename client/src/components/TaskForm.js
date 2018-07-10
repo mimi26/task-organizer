9f1196c03387bb75534e43f0f6e95899b6a8a426
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-07-09 20:14:30 -0400

    toggle display status of description input

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -5,21 +5,23 @@ class TaskForm extends Component {
         super(props);
         this.state = {
             title: props.taskToEdit ? props.taskToEdit.title : '',
-            task: props.taskToEdit ? props.taskToEdit.task : ''
+            task: props.taskToEdit ? props.taskToEdit.task : '',
+            displayState: 'none'
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
-
-    // getUserId() {
-    //   this.setState({ user_id: localStorage.getItem('id') });
-    // }
 
     handleInputChange(e) {
         const name = e.target.name;
         const val = e.target.value;
         this.setState({
-            [name]: val,
+            [name]: val
         });
+        if (e.target.value === '') {
+            this.setState({ displayState: 'none' });
+        } else {
+            this.setState({ displayState: 'inline-block' });
+        }
     }
 
     render() {
@@ -28,19 +30,22 @@ class TaskForm extends Component {
                 ? e => this.props.handleTaskSubmit(e, 'POST', this.state)
                 : e => this.props.handleTaskSubmit(e, 'PUT', this.state, this.props.taskToEdit.id)}
                 className="task-form">
-                <input   type="text" 
-                        onChange={this.handleInputChange} 
+                <input  type="text" 
+                        onChange={this.handleInputChange}
                         value={this.state.title}
                         name="title" 
                         placeholder="ADD A NEW TASK"
                         className="task-input"/>
-                <input   type="text" 
+                <input  type="text" 
                         onChange={this.handleInputChange} 
                         value={this.state.task} 
-                        name="task" />
+                        name="task" 
+                        placeholder="ADD A NEW TASK DESCRIPTION"
+                        className="task-input"
+                        style={{display:`${this.state.displayState}`}} />
                 <input  type="submit" 
                         value="submit"
-                        style={{display:"none"}}/>
+                        style={{display:'none'}}/>
             </form>                
         );
     }
